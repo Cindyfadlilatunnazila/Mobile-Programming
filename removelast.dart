0196594aec7131e4df10583e08c6e5240580dded
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:55:14 +0700

    Add files via upload

---
+++ removelast.dart
@@ -0,0 +1,7 @@
+void main() {
+    var list1 = [12,34,65,76,80];
+    print("List before removing element : ${list1}");
+
+    list1.removeLast();
+    print("List after removed element : ${list1}");
+}
