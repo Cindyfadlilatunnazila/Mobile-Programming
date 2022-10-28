78c4acc9bcd725afb2334c4da12436d3fa352ced
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:43:53 +0700

    Add files via upload

---
+++ mapoperation.dart
@@ -0,0 +1,8 @@
+void main(){
+    var student = new  Map();
+    student['name'] = 'Tom';
+    student['age'] = 23;
+    student['course'] = 'B.tech';
+    student['Branch'] = 'Computer Science';
+    print(student);
+}
