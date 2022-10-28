2ef64891d623e9b73c6c2d2549618bcf8cc38ba1
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:45:37 +0700

    Add files via upload

---
+++ openhouse.dart
@@ -0,0 +1,11 @@
+void main (){
+var openHours =8;
+var closedHours =21;
+var now = 7.5; 
+
+if(now> openHours && now < closedHours){
+    print("Hello, Saya Lagi Buka");
+}else {
+    print("Maaf, Kami Sudah Tutup");
+}
+}
