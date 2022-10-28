0bd6136406bea75c05bd48c3e9a712190397a8cf
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:54:16 +0700

    Add files via upload

---
+++ rainbow.dart
@@ -0,0 +1,10 @@
+enum Rainbow { red, orange, blue, yellow, green, indigo, violet }
+
+enum weather { sunny, cloudy, rain, storm }
+
+main() {
+    print(Rainbow.values);
+    print(Rainbow.blue);
+    print(Rainbow.blue.index);
+    print(Rainbow.blue.name);
+}
