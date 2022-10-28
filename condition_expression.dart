a03afd633350bb566b075a3bdbb5622c36b376a2
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:22:41 +0700

    Add files via upload

---
+++ condition_expression.dart
@@ -0,0 +1,10 @@
+void main(){
+     var jamsekarang = 7.8;
+     var jambuka = 8;
+     var statustoko = jamsekarang>jambuka?"Toko Sudah Buka":"Toko Masih tutup";
+
+     print("kondisi toko saat ini : $statustoko");
+     var nama;
+     var pembeli = nama ?? "pembeli umum";
+     print("Nama Pembeli : $pembeli");
+}
