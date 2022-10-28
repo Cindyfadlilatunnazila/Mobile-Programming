e807f6b6cc5d5930a0ef9bac6bad11be65286bf1
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:38:04 +0700

    Add files via upload

---
+++ growable.dart
@@ -0,0 +1,7 @@
+void main() {
+    var list1 = new List.empty(growable:true);
+    list1.add(10);  list1.add(11);
+    list1.add(12);  list1.add(13);
+    list1.add(14);  list1.add(15);
+    print(list1);
+}
