0b3cbfc18677b48fc52018913d23136cf6474202
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:39:55 +0700

    Add files via upload

---
+++ interatingover.dart
@@ -0,0 +1,7 @@
+void main(){
+    print("Example - Remove Element in the given Set");
+    var names = <String>{"Peter", "John", "Ricky", "Devansh","Finch"};
+    names.forEach((value){
+        print("value: $value");
+    });
+}
