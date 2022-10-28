cbab75aad1b9b294f893b50fcb02e7dbec95316d
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:55:56 +0700

    Add files via upload

---
+++ removerange.dart
@@ -0,0 +1,6 @@
+void main() {
+    var list1 = [12,34,65,76,80];
+    print("List before removing element : ${list1}");
+    list1.removeRange(1,3);
+    print("List after removed element : ${list1}");
+}
