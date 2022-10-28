ec961a82e385aa0b2416096e65c624dcadbb7a01
Cindyfadlilatunnazila <86311441+Cindyfadlilatunnazila@users.noreply.github.com>
2022-10-28 08:38:30 +0700

    Add files via upload

---
+++ HigherOrderFunction.dart
@@ -0,0 +1,9 @@
+void myHigherOrderFunction(String message, Function f){
+    print(message);
+    print(f(3, 17));
+}
+
+main() {
+    var sum = (int num1, int num2) => num1 + num2;
+    myHigherOrderFunction('Cindy',sum);
+}
